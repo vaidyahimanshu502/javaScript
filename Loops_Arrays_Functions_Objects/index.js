58200e2d3e61c4dd85fe40b7e481917fb59c1df5
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-10-06 16:45:50 +0530

    Completed Loops with some basics problems.

---
+++ Loops_Arrays_Functions_Objects/index.js
@@ -0,0 +1,28 @@
+// For Loop:
+//Printing prime Numbers-----
+for (let i = 1; i < 50; i += 2) {
+  console.log(i);
+}
+
+// While Loop:
+let i = 0;
+while (i < 5) {
+  console.log("HEllo, Himanshu!");
+  i++;
+}
+
+// print digits
+let num = 1234567;
+while (num !== 0) {
+  let digit = num % 10;
+  num = parseInt(num / 10);
+  console.log(digit);
+}
+
+// Do While Loop:
+
+let d = 0;
+do {
+  console.log("Hello, Himanshu!");
+  d++;
+} while (d <= 3);
