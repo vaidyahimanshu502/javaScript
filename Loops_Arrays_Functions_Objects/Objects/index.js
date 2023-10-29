005ba86d890823853f05077cbc0df5375ccfdedd
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-10-29 09:45:52 +0530

    solved problems of objects

--- Loops_Arrays_Functions_Objects/Objects/index.js
+++ Loops_Arrays_Functions_Objects/Objects/index.js
@@ -47,3 +47,46 @@ function findClassTopper(studentMarks) {
 console.log(findClassTopper(studentMarks));
 // Output: "Jane"
 
+
+//complete the calculatePrice function
+//Do not alter the starter code
+const goods = {
+    apple: { price: 150, quantity: 2 },
+    banana: { price: 75, quantity: 3 },
+    orange: { price: 125, quantity: 1 }
+    };    
+    function calculatePrice(goods){
+        //Implement your function here
+        var totalPrice = 0;
+        for (let goodsName in goods) {
+            totalPrice += goods[goodsName].quantity * goods[goodsName].price;
+        }
+        return totalPrice
+    }
+    console.log(calculatePrice(goods));
+    //output : 650
+
+
+    //Complete the URLconstructor function 
+// Do not change the starter code. 
+
+function URLconstructor(){
+    //Implement Your function here.
+        let finalurl = "";
+            finalurl += "https://";
+            finalurl += domain;
+            finalurl += path;
+            for( let key in queryParameters){
+                finalurl += '?';
+                finalurl += key;
+                finalurl += '=';
+                finalurl += queryParameters[key];
+            }
+            return finalurl; 
+    
+    }
+    const queryParameters = {name:'John',age:'28'}; 
+    const domain = "google.com";
+    const path = '/search';
+    console.log(URLconstructor(queryParameters,domain,path));
+    //output: https://google.com/search?name=John&age=28
