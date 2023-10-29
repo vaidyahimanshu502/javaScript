65c6a54b97654c0ed3571de37071d2ea17cde016
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-10-29 09:32:51 +0530

    Solved problem by usng objects properties

--- Loops_Arrays_Functions_Objects/Objects/index.js
+++ Loops_Arrays_Functions_Objects/Objects/index.js
@@ -16,3 +16,34 @@ console.log(students);
 for (let key in students) {
   console.log(key + " : " + students[key]);
 }
+
+
+// Problems
+const studentMarks = {
+    John: [85, 90, 92, 88, 87],
+    Jane: [92, 95, 89, 91, 94],
+    David: [78, 85, 90, 92, 84],
+    Emily: [90, 88, 92, 87, 91],
+    Michael: [86, 92, 90, 89, 94]
+};
+
+function findClassTopper(studentMarks) {
+    let highestAverage = -Infinity;
+    let topper = "";
+
+    for (let studentName in studentMarks) {
+        let total = studentMarks[studentName].reduce((a, b) => a + b, 0);
+        let average = total / studentMarks[studentName].length;
+
+        if (average > highestAverage) {
+            highestAverage = average;
+            topper = studentName;
+        }
+    }
+
+    return topper;
+}
+
+console.log(findClassTopper(studentMarks));
+// Output: "Jane"
+
